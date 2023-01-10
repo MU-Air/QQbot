8cdd3fa0df8bfef2c11cead87c466b7269b56203
MU-Air <108199649+MU-Air@users.noreply.github.com>
2023-01-10 00:43:24 +0000

    Pending changes exported from your codespace

---
+++ demo/robot.py
@@ -0,0 +1,37 @@
+import asyncio
+import json
+import os.path
+import threading
+from typing import Dict, List
+
+import aiohttp
+import qqbot
+
+from qqbot.core.util.yaml_util import YamlUtil
+from qqbot.model.message import MessageEmbed, MessageEmbedField, MessageEmbedThumbnail, CreateDirectMessageRequest, \
+    MessageArk, MessageArkKv, MessageArkObj, MessageArkObjKv
+
+test_config = YamlUtil.read(os.path.join(os.path.dirname(__file__), "config.yaml"))
+async def _message_handler(event, message: qqbot.Message):
+    """
+    定义事件回调的处理
+    :param event: 事件类型
+    :param message: 事件对象（如监听消息是Message对象）
+    """
+    msg_api = qqbot.AsyncMessageAPI(t_token, False)
+    # 打印返回信息
+    qqbot.logger.info("event %s" % event + ",receive message %s" % message.content)
+
+    # 发送消息告知用户
+    message_to_send = qqbot.MessageSendRequest(content="依托答辩", msg_id=message.id)
+    await msg_api.post_message(message.channel_id, message_to_send)
+
+
+# async的异步接口的使用示例
+if __name__ == "__main__":
+    t_token = qqbot.Token(test_config["token"]["appid"], test_config["token"]["token"])
+    # @机器人后推送被动消息
+    qqbot_handler = qqbot.Handler(
+        qqbot.HandlerType.AT_MESSAGE_EVENT_HANDLER, _message_handler
+    )
+    qqbot.async_listen_events(t_token, False, qqbot_handler)
